b2e65a41fb92ebd7866e3b17e724d353341fa364
yuliiiah <horbenko.yuliya@gmail.com>
2022-02-24 21:33:54 +0200

    Section 2: JavaScript Language Fundamentals | String methods and concatenation

--- js-sandbox/app.js
+++ js-sandbox/app.js
@@ -1,28 +1,63 @@
-const num1 = 100;
-const num2 = 50;
+const firstName = 'William';
+const lastName = 'Johnson';
+const age = 36;
+const str = 'Hello there my name is Brad';
+const tags = 'web design,web development,programming';
+
 let val;
 
-// Simple math with numbers
-val = num1 + num2;
-val = num1 + num2;
-val = num1 * num2;
-val = num1 - num2;
-val = num1 / num2;
-val = num1 % num2;
+val = firstName + lastName;
 
-// Math Object
-val = Math.PI;
-val = Math.E;
-val = Math.round(2.4);
-val = Math.ceil(2.4);
-val = Math.floor(2.8);
-val = Math.sqrt(64);
-val = Math.abs(-3);
-val = Math.pow(8, 2);
-val = Math.min(2, 33, 4, 6, -7);
-val = Math.max(2, 33, 4, 6, -7);
-val = Math.random();
+// Concatenation
+val = firstName + ' ' + lastName;
 
-val = Math.floor(Math.random() * 20 + 1);
+// Append
+val = 'Bread ';
+val += 'Traversy';
+
+val = 'Hello, my name is ' + firstName + ' and I am ' + age;
+
+// Escaping
+val = "That's awesome, I can't wait";
+
+// length
+val = firstName.length;
+
+// concat
+val = firstName.concat(' ', lastName);
+
+// Change case
+val = firstName.toUpperCase();
+val = firstName.toLowerCase();
+
+// Strings as arrays
+val = firstName[0];
+
+// indexOf(), lastIndexOf()
+val = firstName.indexOf('l');
+val = firstName.lastIndexOf('l');
+
+// charAt()
+val = firstName.charAt('2');
+
+// Get last char
+val = firstName.charAt(firstName.length - 1);
+
+// substring()
+val = firstName.substring(0, 4);
+
+// slice()
+val = firstName.slice(0, 4);
+val = firstName.slice(-3);
+
+// split()
+val = str.split(' ');
+val = tags.split(',');
+
+// replace()
+val = str.replace('Brad', 'Jack');
+
+// iincludes()
+val = str.includes('Hello');
 
 console.log(val);
