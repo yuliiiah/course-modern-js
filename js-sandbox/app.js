09c4ee40ff48d9ea952c7414e1685dbd1c9fc079
yuliiiah <horbenko.yuliya@gmail.com>
2022-02-24 21:43:57 +0200

    Section 2: JavaScript Language Fundamentals | Template literals

--- js-sandbox/app.js
+++ js-sandbox/app.js
@@ -1,63 +1,35 @@
-const firstName = 'William';
-const lastName = 'Johnson';
-const age = 36;
-const str = 'Hello there my name is Brad';
-const tags = 'web design,web development,programming';
+const name = 'John';
+age = 30;
+const job = 'Web Developer';
+city = 'Miami';
 
-let val;
+// Without template strings (es5)
+html =
+  '<ul><li>Name: ' +
+  name +
+  '</li><li>Age: ' +
+  age +
+  '</li><li>Job: ' +
+  job +
+  '</li><li>City: ' +
+  city +
+  ' </li></ul>';
 
-val = firstName + lastName;
+// With template strings (es6)
+function hello() {
+  return 'hello';
+}
 
-// Concatenation
-val = firstName + ' ' + lastName;
+html = `
+  <ul>
+    <li>Name: ${name}</li>
+    <li>Age: ${age}</li>
+    <li>Job: ${job}</li>
+    <li>City: ${city}</li>
+    <li>${2 + 2}</li>
+    <li>${hello()}</li>
+    <li>${age > 30 ? 'Over 30' : 'Under 30'}</li>
+  </ul>
+`;
 
-// Append
-val = 'Bread ';
-val += 'Traversy';
-
-val = 'Hello, my name is ' + firstName + ' and I am ' + age;
-
-// Escaping
-val = "That's awesome, I can't wait";
-
-// length
-val = firstName.length;
-
-// concat
-val = firstName.concat(' ', lastName);
-
-// Change case
-val = firstName.toUpperCase();
-val = firstName.toLowerCase();
-
-// Strings as arrays
-val = firstName[0];
-
-// indexOf(), lastIndexOf()
-val = firstName.indexOf('l');
-val = firstName.lastIndexOf('l');
-
-// charAt()
-val = firstName.charAt('2');
-
-// Get last char
-val = firstName.charAt(firstName.length - 1);
-
-// substring()
-val = firstName.substring(0, 4);
-
-// slice()
-val = firstName.slice(0, 4);
-val = firstName.slice(-3);
-
-// split()
-val = str.split(' ');
-val = tags.split(',');
-
-// replace()
-val = str.replace('Brad', 'Jack');
-
-// iincludes()
-val = str.includes('Hello');
-
-console.log(val);
+document.body.innerHTML = html;
