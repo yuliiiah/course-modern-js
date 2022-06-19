f3bdc42bca555fe6d6396d2fa84937f3bf00febc
yuliiiah <horbenko.yuliya@gmail.com>
2022-06-19 21:48:03 +0200

    Section 2: JavaScript Language Fundamentals | Arrays & Array Methods

--- js-sandbox/app.js
+++ js-sandbox/app.js
@@ -1,35 +1,61 @@
-const name = 'John';
-age = 30;
-const job = 'Web Developer';
-city = 'Miami';
+// Create some arrays
 
-// Without template strings (es5)
-html =
-  '<ul><li>Name: ' +
-  name +
-  '</li><li>Age: ' +
-  age +
-  '</li><li>Job: ' +
-  job +
-  '</li><li>City: ' +
-  city +
-  ' </li></ul>';
+const numbers = [43, 56, 33, 23, 44, 36, 5];
+const numbers2 = new Array(22, 45, 33, 76, 54);
+const fruit = ['Apple', 'Banana', 'Orange', 'Pear'];
+const mixed = [22, 'Hello', true, undefined, null, { a: 1, b: 1 }, new Date()];
 
-// With template strings (es6)
-function hello() {
-  return 'hello';
+let val;
+
+// Get array length
+val = numbers.length;
+// Check if is array
+val = Array.isArray(numbers);
+// Get single value
+val = numbers[3];
+val = numbers[0];
+// Insert into array
+numbers[2] = 100;
+// Find index of value
+val = numbers.indexOf(36);
+
+// MUTATING ARRAYS
+// // Add onto end
+// numbers.push(250);
+// // Add onto front
+// numbers.unshift(120);
+// // Take off from end
+// numbers.pop();
+// // Take off from front
+// numbers.shift();
+// // Splice values
+// numbers.splice(1, 3);
+// // Reverse array
+// numbers.reverse();
+
+// CONCATENATE ARRAYS
+val = numbers.concat(numbers2);
+
+// Sort
+val = fruit.sort();
+// val = numbers.sort();
+
+// // Use the "compare function"
+// val = numbers.sort(function (x, y) {
+//   return x - y;
+// });
+
+// // Reverse sort
+// val = numbers.sort(function (x, y) {
+//   return y - x;
+// });
+
+// Find
+function over50(num) {
+  return num > 50;
 }
 
-html = `
-  <ul>
-    <li>Name: ${name}</li>
-    <li>Age: ${age}</li>
-    <li>Job: ${job}</li>
-    <li>City: ${city}</li>
-    <li>${2 + 2}</li>
-    <li>${hello()}</li>
-    <li>${age > 30 ? 'Over 30' : 'Under 30'}</li>
-  </ul>
-`;
+val = numbers.find(over50);
 
-document.body.innerHTML = html;
+console.log(numbers);
+console.log(val);
