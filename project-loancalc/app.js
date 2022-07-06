b0fbb0bb044871c2194a6402d380cee8d24fd5bf
yuliiiah <horbenko.yuliya@gmail.com>
2022-07-06 21:38:44 +0200

    Section 4: DOM Projects | Loan Calculator [Pt.3] - Loader & User Experience

--- project-loancalc/app.js
+++ project-loancalc/app.js
@@ -1,10 +1,18 @@
 // Listen for submin
-document
-  .getElementById('loan-form')
-  .addEventListener('submit', calculateResults);
+document.getElementById('loan-form').addEventListener('submit', function (e) {
+  // Hide results
+  document.getElementById('results').style.display = 'none';
+
+  // Show loader
+  document.getElementById('loading').style.display = 'block';
+
+  setTimeout(calculateResults, 2000);
+
+  e.preventDefault();
+});
 
 // Calculate results
-function calculateResults(e) {
+function calculateResults() {
   console.log('Calculating...');
 
   // UI vars
@@ -27,15 +35,25 @@ function calculateResults(e) {
     monthlyPayment.value = monthly.toFixed(2);
     totalPayment.value = (monthly * calculatedPayments).toFixed(2);
     totalInterest.value = (monthly * calculatedPayments - principal).toFixed(2);
+
+    // Hide loader
+    document.getElementById('loading').style.display = 'none';
+
+    // Show results
+    document.getElementById('results').style.display = 'block';
   } else {
     showError('Please check your numbers');
   }
-
-  e.preventDefault();
 }
 
 // Show error
 function showError(error) {
+  // Hide loader
+  document.getElementById('loading').style.display = 'none';
+
+  // Hide results
+  document.getElementById('results').style.display = 'none';
+
   // Create a div
   const errorDiv = document.createElement('div');
 
