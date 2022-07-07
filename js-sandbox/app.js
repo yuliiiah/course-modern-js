b61c6dc25f357df963919ea9021e2ca8be76a7cb
yuliiiah <horbenko.yuliya@gmail.com>
2022-07-07 23:21:58 +0200

    Section 5: Object Oriented JavaScript - ES5 & ES2015 | Built In Constructors

--- js-sandbox/app.js
+++ js-sandbox/app.js
@@ -1,21 +1,44 @@
-// Person constructor
-function Person(name, dob) {
-  this.name = name;
-  // this.age = age;
-  this.birthday = new Date(dob);
+// String
+const name1 = 'Jeff';
+const name2 = new String('Jeff');
 
-  this.calculateAge = function () {
-    const diff = Date.now() - this.birthday.getTime();
-    const ageDate = new Date(diff);
+// name2.foo = 'bar';
+// console.log(name2);
 
-    return Math.abs(ageDate.getUTCFullYear() - 1970);
-  };
+// console.log(typeof name2);
+
+if (name2 === 'Jeff') {
+  console.log('YES');
+} else {
+  console.log('NO');
 }
 
-// const brad = new Person('Brad', 36);
-// const john = new Person('John', 30);
+// Number
+const num1 = 5;
+const num2 = new Number(5);
 
-// console.log(brad);
+// Bool
+const bool1 = true;
+const bool2 = new Boolean(true);
 
-const brad = new Person('Brad', '9-10-1981');
-console.log(brad.calculateAge());
+// Function
+const getSum1 = function (x, y) {
+  return x + y;
+};
+
+const getSum2 = new Function('x', 'y', 'return 1 + 1');
+
+// Object
+const john = { name: 'John' };
+const john2 = new Object({ name: 'John' });
+
+// Arrays
+const arr1 = [1, 2, 3, 4];
+const arr2 = new Array(1, 2, 3, 4);
+
+// Regular expressions
+const re1 = /\w+/;
+const re2 = new RegExp('/w+/');
+
+console.log(re1);
+console.log(re2);
