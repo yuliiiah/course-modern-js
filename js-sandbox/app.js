e6043dc18b3e16de2ffe28751550c845ef8476d2
yuliiiah <horbenko.yuliya@gmail.com>
2022-07-07 23:06:31 +0200

    Section 5: Object Oriented JavaScript - ES5 & ES2015 | Constructors & the 'this' Keyword

--- js-sandbox/app.js
+++ js-sandbox/app.js
@@ -1,44 +1,21 @@
-// Set local storage item
-// localStorage.setItem('name', 'John');
-// localStorage.setItem('age', '69');
+// Person constructor
+function Person(name, dob) {
+  this.name = name;
+  // this.age = age;
+  this.birthday = new Date(dob);
 
-// Set session storage item
-// sessionStorage.setItem('name', 'Beth');
+  this.calculateAge = function () {
+    const diff = Date.now() - this.birthday.getTime();
+    const ageDate = new Date(diff);
 
-// Remove from local storage
-// localStorage.removeItem('name');
+    return Math.abs(ageDate.getUTCFullYear() - 1970);
+  };
+}
 
-// Get from storage
-// const name = localStorage.getItem('name');
-// const age = localStorage.getItem('age');
+// const brad = new Person('Brad', 36);
+// const john = new Person('John', 30);
 
-// // Clear local storage
-// localStorage.clear();
+// console.log(brad);
 
-// console.log(name, age);
-
-document.querySelector('form').addEventListener('submit', function (e) {
-  const task = document.getElementById('task').value;
-
-  let tasks;
-
-  if (localStorage.getItem('tasks') === null) {
-    tasks = [];
-  } else {
-    tasks = JSON.parse(localStorage.getItem('tasks'));
-  }
-
-  tasks.push(task);
-
-  localStorage.setItem('task', JSON.stringify(tasks));
-
-  // alert('Task Created');
-
-  e.preventDefault();
-});
-
-const tasks = JSON.parse(localStorage.getItem('tasks'));
-
-tasks.forEach(function (task) {
-  console.log(task);
-});
+const brad = new Person('Brad', '9-10-1981');
+console.log(brad.calculateAge());
