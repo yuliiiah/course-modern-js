6d0e23876f1162fd842058bed0697d4d2618cd6a
yuliiiah <horbenko.yuliya@gmail.com>
2022-07-07 23:49:53 +0200

    Section 5: Object Oriented JavaScript - ES5 & ES2015 | ES6 Classes

--- js-sandbox/app.js
+++ js-sandbox/app.js
@@ -1,25 +1,38 @@
-const personPrototypes = {
-  greeting: function () {
-    return `Hello there ${this.firstName} ${this.lastName}`;
-  },
-  getsMarried: function (newLastName) {
-    this.lastName = newLastName;
-  },
-};
+class Person {
+  constructor(firstName, lastName, dob) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.birthday = new Date(dob);
+  }
 
-const mary = Object.create(personPrototypes);
-mary.firstName = 'Mary';
-mary.lastName = 'Williams';
-mary.age = 30;
+  greeting() {
+    return `Hello there ${this.firstName}`;
+  }
+
+  calculateAge() {
+    const diff = Date.now() - this.birthday.getTime();
+    const ageDate = new Date(diff);
+
+    return Math.abs(ageDate.getUTCFullYear() - 1970);
+  }
+
+  getsMarried(newLastName) {
+    this.lastName = newLastName;
+  }
+
+  static addNumbers(x, y) {
+    return x + y;
+  }
+}
+
+const mary = new Person('Mary', 'Williams', '11-13-1980');
+
+console.log(mary);
+console.log(mary.greeting());
+console.log(mary.calculateAge());
 
 mary.getsMarried('Thompson');
 
-console.log(mary.greeting());
+console.log(mary);
 
-const brad = Object.create(personPrototypes, {
-  firstName: { value: 'Brad' },
-  lastName: { value: 'Traversy' },
-  age: { value: 20 },
-});
-
-console.log(brad);
+console.log(Person.addNumbers(1, 2));
