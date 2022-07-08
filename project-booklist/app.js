d7e63f552c504c70ebd2d7eb6e1b1e490131949b
yuliiiah <horbenko.yuliya@gmail.com>
2022-07-08 23:25:48 +0200

    Section 6: OOP Book List Project | Delete Book From List

--- project-booklist/app.js
+++ project-booklist/app.js
@@ -46,6 +46,13 @@ UI.prototype.showAlert = function (message, className) {
   }, 3000);
 };
 
+// Delete book
+UI.prototype.deleteBook = function (target) {
+  if (target.className === 'delete') {
+    target.parentElement.parentElement.remove();
+  }
+};
+
 // Clear fields
 UI.prototype.clearFields = function () {
   document.getElementById('title').value = '';
@@ -53,7 +60,7 @@ UI.prototype.clearFields = function () {
   document.getElementById('isbn').value = '';
 };
 
-// Event listeners
+// Event listener for add book
 document.getElementById('book-form').addEventListener('submit', function (e) {
   // Get form values
   const title = document.getElementById('title').value,
@@ -82,4 +89,18 @@ document.getElementById('book-form').addEventListener('submit', function (e) {
   }
 
   e.preventDefault();
+});
+
+// Event listener for delete
+document.getElementById('book-list').addEventListener('click', function (e) {
+  // Instantiate a UI object
+  const ui = new UI();
+
+  // Delete the book
+  ui.deleteBook(e.target);
+
+  // Show alert
+  ui.showAlert('Book removed!', 'success');
+
+  e.preventDefault;
 });
