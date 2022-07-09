be2b0ced6cd66561ea67de2e52b0785c1da70ff0
yuliiiah <horbenko.yuliya@gmail.com>
2022-07-09 20:52:04 +0200

    Section 7: Asynchronous JavaScript, Ajax & Fetch API | Working With Ajax & JSON

--- js-sandbox/app.js
+++ js-sandbox/app.js
@@ -1,44 +1,58 @@
-document.getElementById('button').addEventListener('click', loadData);
+document.getElementById('button1').addEventListener('click', loadCustomer);
+document.getElementById('button2').addEventListener('click', loadCustomers);
 
-function loadData() {
-  // Create an XHR Object
+function loadCustomer(e) {
   const xhr = new XMLHttpRequest();
 
-  // Open
-  xhr.open('GET', 'data.txt', true);
-  console.log('READYSTATE', xhr.readyState);
-
-  // Optional - Used for spinners / loaders
-  xhr.onprogress = function () {
-    console.log('READYSTATE', xhr.readyState);
-  };
+  xhr.open('GET', 'customer.json', true);
 
   xhr.onload = function () {
-    console.log('READYSTATE', xhr.readyState);
-
     if (this.status === 200) {
       // console.log(this.responseText);
-      document.getElementById(
-        'output'
-      ).innerHTML = `<h1>${this.responseText}</h1>`;
+
+      const customer = JSON.parse(this.responseText);
+
+      const output = `
+      <ul>
+      <li>ID: ${customer.id}</li>
+      <li>Name: ${customer.name}</li>
+      <li>Company: ${customer.company}</li>
+      <li>Phone: ${customer.phone}</li>
+      </ul>`;
+
+      document.getElementById('customer').innerHTML = output;
     }
   };
 
-  xhr.onerror = function () {
-    console.log('Request error...');
+  xhr.send();
+}
+
+function loadCustomers(e) {
+  const xhr = new XMLHttpRequest();
+
+  xhr.open('GET', 'customers.json', true);
+
+  xhr.onload = function () {
+    if (this.status === 200) {
+      // console.log(this.responseText);
+
+      const customers = JSON.parse(this.responseText);
+
+      let output = '';
+
+      customers.forEach((customer) => {
+        output += `
+        <ul>
+        <li>ID: ${customer.id}</li>
+        <li>Name: ${customer.name}</li>
+        <li>Company: ${customer.company}</li>
+        <li>Phone: ${customer.phone}</li>
+        </ul>`;
+      });
+
+      document.getElementById('customers').innerHTML = output;
+    }
   };
 
   xhr.send();
-
-  // HTTP Statuses
-  // - 200: OK
-  // - 403: Forbidden
-  // - 404: Not Found
-
-  // readyState Values
-  // 0: request not initialized
-  // 1: server connection established
-  // 2: request received
-  // 3: processing request
-  // 4: request finished and response is ready
 }
