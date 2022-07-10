70b9f82c8d2d18b284d2892ed86139559b701cea
yuliiiah <horbenko.yuliya@gmail.com>
2022-07-10 20:59:51 +0200

    Section 7: Asynchronous JavaScript, Ajax & Fetch API | Custom HTTP Library (Ajax With Callbacks) - Part 2

--- project-easyhttp/app.js
+++ project-easyhttp/app.js
@@ -1,10 +1,66 @@
 const http = new easyHTTP();
 
 // Get posts
-http.get('https://jsonplaceholder.typicode.com/posts', function (error, posts) {
-  if (error) {
-    console.log(error);
-  } else {
-    console.log(posts);
+// http.get('https://jsonplaceholder.typicode.com/posts', function (error, posts) {
+//   if (error) {
+//     console.log(error);
+//   } else {
+//     console.log(posts);
+//   }
+// });
+
+// Get a single post
+// http.get(
+//   'https://jsonplaceholder.typicode.com/posts/1',
+//   function (error, post) {
+//     if (error) {
+//       console.log(error);
+//     } else {
+//       console.log(post);
+//     }
+//   }
+// );
+
+// Create data
+const data = {
+  title: 'Custom post',
+  body: 'This is a custom post',
+};
+
+// Create post
+// http.post(
+//   'https://jsonplaceholder.typicode.com/posts',
+//   data,
+//   function (err, post) {
+//     if (err) {
+//       console.log(err);
+//     } else {
+//       console.log(post);
+//     }
+//   }
+// );
+
+// Update a post
+// http.put(
+//   'https://jsonplaceholder.typicode.com/posts/1',
+//   data,
+//   function (err, post) {
+//     if (err) {
+//       console.log(err);
+//     } else {
+//       console.log(post);
+//     }
+//   }
+// );
+
+// Delete a post
+http.delete(
+  'https://jsonplaceholder.typicode.com/posts/1',
+  function (error, response) {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log(response);
+    }
   }
-});
+);
