b6cbccf0f0ccc4cc659b3b34eda1121711c80b0c
yuliiiah <horbenko.yuliya@gmail.com>
2022-07-11 20:19:55 +0200

    Section 8: API Projects | GitHub Finder [Pt.2] - Fetching Profile Data

---
+++ project-github-finder/github.js
@@ -0,0 +1,18 @@
+class GitHub {
+  constructor() {
+    this.client_id = '1f51ef08f0d2f9db17c0';
+    this.client_secret = 'f68a4cf54a960c33141416452d05a30a1e97d0ce';
+  }
+
+  async getUser(user) {
+    const profileResponse = await fetch(
+      `https://api.github.com/users/${user}?client_id=${this.client_id}$client_secret=${this.client_secret}`
+    );
+
+    const profileData = await profileResponse.json();
+
+    return {
+      profile,
+    };
+  }
+}
