ec44b978e225f6764085af032332e63c4b60deed
yuliiiah <horbenko.yuliya@gmail.com>
2022-07-12 21:13:03 +0200

    Section 8: API Projects | WeatherJS [Pt.2] - Fetch Weather From API

---
+++ project-weather-js/weather.js
@@ -0,0 +1,24 @@
+class Weather {
+  constructor(city, country) {
+    this.key = '4cba16c0bf694135a30182646221207';
+    this.city = city;
+    this.country = country;
+  }
+
+  // Fetch weather from API
+  async getWeather() {
+    const response = await fetch(
+      `http://api.weatherapi.com/v1/current.json?key=${this.key}&q=${this.city}&country=${this.country}&aqi=no`
+    );
+
+    const responseData = await response.json();
+
+    return responseData.current;
+  }
+
+  // Change weather location
+  changeWeatherLocation(city, country) {
+    this.city = city;
+    this.country = country;
+  }
+}
