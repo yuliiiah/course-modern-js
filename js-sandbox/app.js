7e6d74a401d457bae0a5457f5e7da41366822251
yuliiiah <horbenko.yuliya@gmail.com>
2022-07-13 22:41:44 +0200

    Section 9: Error Handling & Regular Expressions | Regular Expressions [Pt.1] - Evaluation Functions

--- js-sandbox/app.js
+++ js-sandbox/app.js
@@ -1,28 +1,32 @@
-const user = { email: 'jdoe@gmail.com' };
+let re;
+re = /hello/; // i = case insensitive
+re = /hello/i; // i = case insensitive
+// re = /hello/g; // g = global search
 
-try {
-  // Produce a ReferenceError
-  // myFunction();
-  // Produce a TypeError
-  // null.myFunction();
-  // Produce a SyntaxError
-  // console.log(eval('Hello World'));
-  // Produce a URIError
-  // decodeURIComponent('%');
+// console.log(re);
+// console.log(re.source);
 
-  if (!user.name) {
-    // throw 'User has no name';
-    throw new SyntaxError('User has no name');
-  }
-} catch (error) {
-  console.log(`User Error: ${error.message}`);
-  // console.log(error);
-  // console.log(error.message);
-  // console.log(error.name);
-  // console.log(error.name);
-  // console.log(error instanceof ReferenceError);
-} finally {
-  console.log('Finally runs regardless of the result...');
-}
+// exec() - Return results in an array or null
+// const result = re.exec('hello world');
 
-console.log('Program continues...');
+// console.log(result);
+// console.log(result[0]);
+// console.log(result.index);
+// console.log(result.input);
+
+// test() - Return true or false
+// const result = re.test('Hello');
+
+// match() - Return result array or null
+// const str = 'Hello There';
+// const result = str.match(re);
+
+// search() - Return index of the first match, if not found returns -1
+// const str = 'Yo Hello There';
+// const result = str.search(str);
+
+// replace() - Return a new string with some or all matches of a pattern
+const str = 'Hello There';
+const newStr = str.replace(re, 'Hi');
+
+console.log(newStr);
