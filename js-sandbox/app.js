f668a43beca5c9de375e3aafec96f490955dd61f
yuliiiah <horbenko.yuliya@gmail.com>
2022-07-14 17:33:49 +0200

    Section 9: Error Handling & Regular Expressions | Regular Expressions [Pt.3] - Character Sets & Quantifiers

--- js-sandbox/app.js
+++ js-sandbox/app.js
@@ -13,8 +13,25 @@ re = /h*llo/i; // Matches character 0 or more times
 re = /gre?a?y/i; // Optional character
 re = /gre?a?y\?/i; // Escape character
 
+// Brackets [] - Character sets
+re = /gr[ae]y/i; // Must be an a or e
+re = /[GF]ray/; // Must be a G or F
+re = /[^GF]ray/i; // Anything excepf for G or F
+re = /[A-Z]ray/; // Any uppercase letter
+re = /[a-z]ray/; // Any lowercase letter
+re = /[A-Za-z]ray/; // Any letter case
+re = /[0-9][0-9]rey/; // Any digit
+
+// Braces {} - Quantifiers
+re = /Hel{2}o/i; // Must occur exactly {m} times
+re = /Hel{2,4}o/i; // Must occur in the {m} range of times
+re = /Hel{2,}o/i; // Must occur at least {m} times
+
+// Parentheses () - Grouping
+re = /^([0-9]x){3}$/;
+
 // String to match
-const string = 'Gray?';
+const string = '3x3x3x';
 // Log results
 const result = re.exec(string);
 console.log(result);
