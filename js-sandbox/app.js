f0da59122c6cbecb58502da6a705e4a233be7dba
yuliiiah <horbenko.yuliya@gmail.com>
2022-07-16 21:54:46 +0200

    Section 10: Other Newer Features - ES2015+ | Destructuring

--- js-sandbox/app.js
+++ js-sandbox/app.js
@@ -1,30 +1,51 @@
-// Create a symbol
-// const sym1 = Symbol();
-// const sym2 = Symbol('sym2');
+// Destructuring Assignment
+let a, b;
 
-// console.log(typeof sym2);
+[a, b] = [100, 200];
 
-// console.log(Symbol() === Symbol());
-// console.log(`Hello ${String(sym1)}`);
+// Rest Pattern
+[a, b, c, ...rest] = [100, 200, 300, 400, 500];
 
-// Unique Object Keys
-const KEY1 = Symbol();
-const KEY2 = Symbol('sym2');
+({ a, b } = { a: 100, b: 200, c: 300, d: 400, e: 500 });
+({ a, b, ...rest } = { a: 100, b: 200, c: 300, d: 400, e: 500 });
 
-const myObj = {};
+// Array Destructuring
+const people = ['John', 'Beth', 'Mike'];
 
-myObj[KEY1] = 'Prop1';
-myObj[KEY2] = 'Prop2';
-myObj.key3 = 'Prop3';
-myObj.key4 = 'Prop4';
+// const [person1, person2, person3] = people;
 
-// console.log(myObj[KEY1]);
+// console.log(person1, person2, person3);
 
-// Symbols are not enumerable in for...in
-// for (let i in myObj) {
-//   console.log(`${i}: ${myObj[i]}`);
-// }
+// Parse array returned from functions
+function getPeople() {
+  return ['John', 'Beth', 'Mike'];
+}
 
-// Symbols are ignored by JSON.stringify()
-console.log(JSON.stringify({ key: 'prop' }));
-console.log(JSON.stringify({ [Symbol('sym1')]: 'prop' }));
+let person1, person2, person3;
+[person1, person2, person3] = getPeople();
+
+// console.log(person1, person2, person3);
+
+// Object Destructuring
+const person = {
+  name: 'John Doe',
+  age: 32,
+  city: 'Miami',
+  gender: 'male',
+
+  sayHello: function () {
+    console.log('Hello');
+  },
+};
+
+// Old ES5
+// const name = person.name,
+//   age = person.age,
+//   city = person.city;
+
+// New ES6 Destructuring
+const { name, age, city, sayHello } = person;
+
+console.log(name, age, city);
+
+sayHello();
