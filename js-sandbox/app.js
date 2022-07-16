f665966c7eb69585ffd6756291a600e3a1524bef
yuliiiah <horbenko.yuliya@gmail.com>
2022-07-16 23:40:38 +0200

    Section 11: JavaScript Patterns | State Pattern - Small Project

--- js-sandbox/app.js
+++ js-sandbox/app.js
@@ -1,53 +1,85 @@
-const User = function (name) {
-  this.name = name;
-  this.chatroom = null;
-};
+const PageState = function () {
+  let currentState = new HomeState(this);
 
-User.prototype = {
-  send: function (message, to) {
-    this.chatroom.send(message, this, to);
-  },
-  receive: function (message, from) {
-    console.log(`${from.name} to ${this.name}: ${message}`);
-  },
-};
+  this.init = function () {
+    this.change(new HomeState());
+  };
 
-const Chatroom = function () {
-  let users = {}; // List of users
-
-  return {
-    register: function (user) {
-      users[user.name] = user;
-      user.chatroom = this;
-    },
-    send: function (message, from, to) {
-      if (to) {
-        // Single user message
-        to.receive(message, from);
-      } else {
-        // Mass message
-        for (key in users) {
-          if (users[key] !== from) {
-            users[key].receive(message, from);
-          }
-        }
-      }
-    },
+  this.change = function (state) {
+    currentState = state;
   };
 };
 
-const brad = new User('Brad');
-const jeff = new User('Jeff');
-const dan = new User('Dan');
-const sarah = new User('Sarah');
+// Home State
+const HomeState = function (page) {
+  document.querySelector('#heading').textContent = null;
+  document.querySelector('#content').innerHTML = `
+    <div class="jumbotron">
+      <h1 class="display-3">Hello, world!</h1>
+      <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+      <hr class="my-4">
+      <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+      <p class="lead">
+        <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+      </p>
+    </div>
+  `;
+};
 
-const chatroom = new Chatroom();
+// About State
+const AboutState = function (page) {
+  document.querySelector('#heading').textContent = 'About Us';
+  document.querySelector('#content').innerHTML = `
+    <p>This is the About page.</p>
+  `;
+};
 
-chatroom.register(brad);
-chatroom.register(jeff);
-chatroom.register(dan);
-chatroom.register(sarah);
+// Contact State
+const ContactState = function (page) {
+  document.querySelector('#heading').textContent = 'Contact Us';
+  document.querySelector('#content').innerHTML = `
+  <form>
+    <div class="form-group">
+      <label>Name</label>
+      <input type="text" class="form-control">
+    </div>
+    <div class="form-group">
+      <label>Email address</label>
+      <input type="email" class="form-control">
+    </div>
+    <button type="submit" class="btn btn-primary">Submit</button>
+  </form>
+  `;
+};
 
-brad.send('Hello Jeff', jeff);
-sarah.send('Hello Brad, you are weird!', brad);
-jeff.send('Hello everyone');
+// Init PageState
+const page = new PageState();
+
+// Init the first state
+page.init();
+
+// UI Vars
+const home = document.getElementById('home'),
+  about = document.getElementById('about'),
+  contact = document.getElementById('contact');
+
+// Home
+home.addEventListener('click', (e) => {
+  page.change(new HomeState());
+
+  e.preventDefault;
+});
+
+// About
+about.addEventListener('click', (e) => {
+  page.change(new AboutState());
+
+  e.preventDefault;
+});
+
+// Home
+contact.addEventListener('click', (e) => {
+  page.change(new ContactState());
+
+  e.preventDefault;
+});
