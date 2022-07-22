2386cc379f3edf7ed81379899fcbae6772e653c8
yuliiiah <horbenko.yuliya@gmail.com>
2022-07-22 21:05:28 +0200

    Section 13: Microposts Project - CRUD Front End (Webpack & Babel) | Get & Display Posts

---
+++ project-microposts/src/http.js
@@ -0,0 +1,55 @@
+/**
+* EasyHTTP Library
+* Library for making HTTP requests
+
+* @version 3.0.0
+* @author yuliiiah
+* @license GOD
+*
+**/
+
+class EasyHTTP {
+  // Make an HTTP GET request
+  async get(url) {
+    const response = await fetch(url);
+    const responseData = await response.json();
+    return responseData;
+  }
+
+  // Make an HTTP POST request
+  async post(url, data) {
+    const response = await fetch(url, {
+      method: 'POST',
+      headers: { 'Content-type': 'Application/json' },
+      body: JSON.stringify(data),
+    });
+
+    const responseData = await response.json();
+    return responseData;
+  }
+
+  // Make an HTTP PUT request
+  async put(url, data) {
+    const response = await fetch(url, {
+      method: 'PUT',
+      headers: { 'Content-type': 'Application/json' },
+      body: JSON.stringify(data),
+    });
+
+    const responseData = await response.json();
+    return responseData;
+  }
+
+  // Make an HTTP DELETE request
+  async delete(url) {
+    const response = await fetch(url, {
+      method: 'DELETE',
+      headers: { 'Content-type': 'Application/json' },
+    });
+
+    const responseData = await 'Resource deleted...';
+    return responseData;
+  }
+}
+
+export const http = new EasyHTTP();
